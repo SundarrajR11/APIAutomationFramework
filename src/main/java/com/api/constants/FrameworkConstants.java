02008daed6eddebfa4992fe5683fe0b62a9f4cb2
Sundarraj R <ragusundar274@gmail.com>
2024-06-09 23:09:12 +0530

    [Optimized] FrameworkConstants Class

--- src/main/java/com/api/constants/FrameworkConstants.java
+++ src/main/java/com/api/constants/FrameworkConstants.java
@@ -1,14 +1,12 @@
 package com.api.constants;
 
 import lombok.Getter;
-
-
-
 public final class FrameworkConstants {
     private FrameworkConstants(){}
-
-    private static final String CWD = System.getProperty("user.dir");
-
-    @Getter private static   final String CONFIG_FILE_PATH = CWD+"/src/test/resources/config/config.properties";
+    private static final String PWD=System.getProperty("user.dir");
+    @Getter private static final String CONFIG_FILE_PATH = PWD+"/src/test/resources/config/config.properties";
+    @Getter private static final String EXT_REQ_FILE_PATH= PWD+"/src/test/resources/externalReqBodies/request-body.json";
+    @Getter private static final String RES_STORE_FOLDER_PATH= PWD+"/output/";
 
 }
+
